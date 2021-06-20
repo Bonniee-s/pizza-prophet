e7dda320edf6d057305d43a66c19f2fe3522991e
Bonnie Shen <Bonnie.by.Shen@gmail.com>
2021-06-20 15:09:14 -0400

    Added form submit listener. grabbed input value

--- script.js
+++ script.js
@@ -32,3 +32,19 @@ const pizza = {
 		}
 	]
 };
+
+// listen for a form submit.
+//const $form = $('form');
+//$form.on('submit') in jquery
+const form = document.querySelector('form');
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	console.log("Form submitted!");
+	//once form submits, grab values from selected radio inputs.
+	const sizeSelected = document.querySelector('input[name=size]:checked').value;
+	const priceSelected = document.querySelector('input[name=price]:checked').value;
+	console.log(sizeSelected, priceSelected);
+	
+})
+//use radio input values to look through pizza object
+//display the matching pizza joint to you 
