d91dbbcf83785b7745d816870a5b8d032359a31c
Bonnie Shen <Bonnie.by.Shen@gmail.com>
2021-06-20 15:48:33 -0400

    Added results to page

--- script.js
+++ script.js
@@ -38,35 +38,51 @@ const randomElement = (array) => {
 	return array[randomIndex];
 }
 
+const pizzaProphetInit = () => {
+		// listen for a form submit.
+	//const $form = $('form');
+	//$form.on('submit') in jquery
+	const form = document.querySelector('form');
+	form.addEventListener('submit', e => {
+		e.preventDefault();
+		//console.log("Form submitted!");
+		//once form submits, grab values from selected radio inputs.
+		const sizeSelected = document.querySelector('input[name=size]:checked').value;
+		const priceSelected = document.querySelector('input[name=price]:checked').value;
+		//console.log(sizeSelected, priceSelected);
 
-// listen for a form submit.
-//const $form = $('form');
-//$form.on('submit') in jquery
-const form = document.querySelector('form');
-form.addEventListener('submit', e => {
-	e.preventDefault();
-	console.log("Form submitted!");
-	//once form submits, grab values from selected radio inputs.
-	const sizeSelected = document.querySelector('input[name=size]:checked').value;
-	const priceSelected = document.querySelector('input[name=price]:checked').value;
-	console.log(sizeSelected, priceSelected);
+		//use radio input values to look through pizza object
+		const sizePizzaList = pizza[sizeSelected];
+		//console.log(sizePizzaList);
 
-	//use radio input values to look through pizza object
-	const sizePizzaList = pizza[sizeSelected];
-	console.log(sizePizzaList);
+		const withinPricePizzaList = sizePizzaList.filter((pizzaPlace) => {
+			//console.log(pizzaPlace);
+			if (pizzaPlace.price === priceSelected) {
+				return true;
+			} else { 
+				return false;
+			}
 
-	const withinPricePizzaList = sizePizzaList.filter((pizzaPlace) => {
-		console.log(pizzaPlace);
-		if (pizzaPlace.price === priceSelected) {
-			return true;
-		} else { 
-			return false;
-		}
+		});
+		//console.log(withinPricePizzaList);
+		
+		const finalPizzaPlace = randomElement(withinPricePizzaList);
+		//console.log("FINAL: ", finalPizzaPlace);
 
-	});
-	console.log(withinPricePizzaList);
-	
-	const finalPizzaPlace = randomElement(withinPricePizzaList);
-	console.log("FINAL: ", finalPizzaPlace);
-})
-//display the matching pizza joint to you 
+		//display the matching pizza joint to you 
+		const pizzaHtml = `
+			<h2>Go here ⏩ <span class="restaurant">${finalPizzaPlace.title}</span></h2>
+		`;
+
+		document.querySelector('.results').innerHTML = pizzaHtml;
+	})
+
+}
+// Vanilla JS document ready
+if (document.readyState === 'complete') {
+	//IE U support 
+	pizzaProphetInit();
+} else {
+	//other modern browsers
+	document.addEventListener('DOMContentLoaded', pizzaProphetInit);
+}
